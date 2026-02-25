dec0f5a6a4f22732d6e37a3d028692fb2f412310
mdsujoncocbd3-cloud <mdsujoncocbd3@gmail.com>
2026-02-26 01:10:51 +0600

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Service Worker for MIX BOX PWA
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // It acts as a dummy proxy to satisfy PWA criteria
-});
